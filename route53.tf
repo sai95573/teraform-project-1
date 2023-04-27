f02e19228a446f4aeaae5a9a70429efe08ddb2fa
saikrishna <sai95573@gmail.com>
2023-04-27 17:28:37 +0530

    terraform files added

---
+++ route53.tf
@@ -0,0 +1,25 @@
+data "aws_route53_zone" "hosted_zone" {
+  name = var.domain_name
+}
+
+locals {
+  web_server_ip = aws_instance.web_server.*.public_ip
+  mail_server_ip = aws_instance.mail_server.*.public_ip
+
+}
+
+resource "aws_route53_record" "web" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.web_sub_domain_name
+  type    = "A"
+  ttl     = 300
+  records = local.web_server_ip
+}
+
+resource "aws_route53_record" "mail" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.mail_sub_domain_name
+  type    = "A"
+  ttl     = 300
+  records = local.mail_server_ip
+}
